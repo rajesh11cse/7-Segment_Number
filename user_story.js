bff2594e2e7cc36875171a6256b53d6c5491c184
Rajesh Kumar <rajeshkumar11cse@gmail.com>
2019-07-04 13:21:24 +0530

    7 segment

--- user_story.js
+++ user_story.js
@@ -12,9 +12,7 @@ function get7segment(ascii) {
       return r;
     }, []). // return array r with type [](array empty on no element)
     map(function (a) {
-      return a.join(''); // remove quotes
-    }).
-    map(function (a) {
+      a = a.join('')
       var bits = { 63: 0, 6: 1, 91: 2, 79: 3, 102: 4, 109: 5, 125: 6, 7: 7, 127: 8, 111: 9, 0: ' ' },
         myvar = '909561432'.split('').reduce(function (r, v, i) {
           return r + (a[i] !== ' ' && 1 << v);
